3697bdbbba03fcd3827deafa48b8512d757a467d
Saksham31-iiitl <mcs24025@iiitl.ac.in>
2025-06-06 08:23:43 +0530

    Update imdb_rated_data_read.py

--- imdb_rated_data_read.py
+++ imdb_rated_data_read.py
@@ -1,21 +1,22 @@
-import pandas as pd  
+import pandas as pd
 
 file_path = 'IMDb_top_rated_data.csv'
 df = pd.read_csv(file_path)
 
-print(df.head())  
-print(df.info())  
-print(df.describe())  
-
-
-
-df['genres'] = df['genres'].str.strip().str.rstrip
+df['genres'] = df['genres'].str.strip().str.rstrip(',')
 df_genres = df.assign(genres=df['genres'].str.split(',')).explode('genres')
-df_genres['genres'] = df_genres['genres'].str.strip()  
+df_genres['genres'] = df_genres['genres'].str.strip()
+
+
+df_genres['releaseYear'] = pd.to_datetime(df_genres['releaseYear'], format='%Y')
+
+
+df_genres['decade'] = (df_genres['releaseYear'].dt.year // 10) * 10
 
 
 df_genres.columns = df_genres.columns.str.lower().str.replace(' ', '_')
 
-df_genres['releaseYear'] = pd.to_datetime(df_genres['releaseYear'], format='%Y')
 
-df_genres['decade'] = (df_genres['releaseYear'].dt.year // 10) * 10
+print(df_genres.head())
+print(df_genres.info())
+print(df_genres.describe())
