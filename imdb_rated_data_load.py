0019091f4e21e1863e5bd5d4aa165a7591109cc2
Saksham31-iiitl <mcs24025@iiitl.ac.in>
2025-06-06 08:18:03 +0530

    Update imdb_rated_data_load.py

--- imdb_rated_data_load.py
+++ imdb_rated_data_load.py
@@ -1,6 +1,6 @@
 import psycopg2
 
-# Define connection parameters
+
 conn = psycopg2.connect(
     dbname="your_dbname",
     user="your_username",
@@ -10,7 +10,6 @@ conn = psycopg2.connect(
 )
 cur = conn.cursor()
 
-# Create the movies table if it doesn't exist
 create_table_query = """
 CREATE TABLE IF NOT EXISTS movies (
     id VARCHAR(10) PRIMARY KEY,
@@ -26,7 +25,6 @@ cur.execute(create_table_query)
 conn.commit()
 
 
-# Insert data into the movies table
 for _, row in df_genres.iterrows():
     insert_query = """
     INSERT INTO movies (id, title, genres, averageRating, numVotes, releaseYear, decade)
@@ -36,6 +34,5 @@ for _, row in df_genres.iterrows():
     cur.execute(insert_query, tuple(row))
 conn.commit()
 
-# Close the cursor and connection
 cur.close()
 conn.close()
